db5d7d56992474a2314047288ea4a836f254aa98
Yury Kurashov <yurikurashov@gmail.com>
2019-04-11 23:43:56 +0300

    ContextMenu

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.Size;
+import android.view.ContextMenu;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +23,19 @@ public class MainActivity extends AppCompatActivity {
     CheckBox checkBox1;
     CheckBox checkBox2;
     MenuItem item1;
+    TextView tvColor, tvSize;
+
+    final int MENU_COLOR_RED = 1;
+    final int MENU_COLOR_GREEN = 2;
+    final int MENU_COLOR_BlUE = 3;
+
+    final int MENU_SIZE_22 = 4;
+    final int MENU_SIZE_26 = 5;
+    final int MENU_SIZE_30 = 6;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,21 +55,77 @@ public class MainActivity extends AppCompatActivity {
 
         checkBox1 = findViewById(R.id.checkBox1);
         checkBox2 = findViewById(R.id.checkBox2);
+        tvColor = findViewById(R.id.textView1);
+        tvSize = findViewById(R.id.textView2);
+
+        registerForContextMenu(tvColor);
+        registerForContextMenu(tvSize);
 
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        switch (v.getId()) {
+            case R.id.textView1:
+                menu.add(0, MENU_COLOR_RED, 0, "Red");
+                menu.add(0, MENU_COLOR_GREEN, 0, "Green");
+                menu.add(0, MENU_COLOR_BlUE, 0, "Blue");
+                break;
+            case R.id.textView2:
+                menu.add (0, MENU_SIZE_22,0 , "22");
+                menu.add (0, MENU_SIZE_26,0 , "26");
+                menu.add (0, MENU_SIZE_30,0 , "30");
+                break;
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case MENU_COLOR_RED:
+                tvColor.setTextColor(Color.RED);
+                tvColor.setText("Text is red");
+                break;
+            case MENU_COLOR_GREEN:
+                tvColor.setTextColor(Color.GREEN);
+                tvColor.setText("Text is green");
+                break;
+            case MENU_COLOR_BlUE:
+                tvColor.setTextColor(Color.BLUE);
+                tvColor.setText("Text is blue");
+                break;
+            case MENU_SIZE_22:
+                tvSize.setTextSize(22);
+                tvSize.setText("Text size is 22");
+                break;
+            case MENU_SIZE_26:
+                tvSize.setTextSize(26);
+                tvSize.setText("Text size is 26");
+                break;
+            case MENU_SIZE_30:
+                tvSize.setTextSize(30);
+                tvSize.setText("Text size is 30");
+                break;
+        }
+        return super.onContextItemSelected(item);
+
+    }
 
     public static final String TAG = "myLogs";
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        item1 = menu.findItem(R.id.action_item1);
-        if (checkBox2.isChecked()){
-            item1.setVisible(true);
-        }
-        else {item1.setVisible(false);
-        }
+//        item1 = menu.findItem(R.id.action_item1);
+//        if (checkBox2.isChecked()){
+//            item1.setVisible(true);
+//        }
+//        else {item1.setVisible(false);
+//        }
         menu.setGroupVisible(R.id.group1, checkBox1.isChecked());
+        menu.setGroupVisible(R.id.group2, checkBox2.isChecked());
       return super.onPrepareOptionsMenu(menu);
     }
 
