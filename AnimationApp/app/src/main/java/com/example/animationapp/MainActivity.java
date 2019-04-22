0cc3eb3837eacd78a1cb829c3ed79dc3c5aa6b7a
Yury Kurashov <yurikurashov@gmail.com>
2019-04-22 23:54:33 +0300

    animation

---
+++ AnimationApp/app/src/main/java/com/example/animationapp/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.animationapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    final int MENU_ALPHA_ID = 1;
+    final int MENU_SCALE_ID = 2;
+    final int MENU_TRANS_ID = 3;
+    final int MENU_ROTATE_ID = 4;
+    final int MENU_COMBO_ID = 5;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        registerForContextMenu(textView);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        switch (v.getId()) {
+            case R.id.textView:
+            menu.add(0, MENU_ALPHA_ID, 0, "Alpha");
+            menu.add(0, MENU_SCALE_ID, 0, "Scale");
+            menu.add(0, MENU_TRANS_ID, 0, "Trans");
+            menu.add(0, MENU_ROTATE_ID, 0, "Rotate");
+            menu.add(0, MENU_COMBO_ID, 0, "Combo");
+            break;
+        }
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        Animation anim = null;
+
+        switch (item.getItemId()) {
+            case MENU_ALPHA_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myalpha);
+                break;
+            case MENU_SCALE_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myscale);
+                break;
+            case MENU_TRANS_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.mytrans);
+                break;
+            case MENU_ROTATE_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myrotate);
+                break;
+            case MENU_COMBO_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.mycombo);
+                break;
+        }
+
+        textView.startAnimation(anim);
+
+        return super.onContextItemSelected(item);
+    }
+}
