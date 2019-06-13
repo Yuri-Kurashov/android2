1c987b07a16d4e09b2cf87b2b73cd8351e644290
Yury Kurashov <yurikurashov@gmail.com>
2019-06-14 00:12:23 +0300

    запосы из двух таблиц

--- SQlite_two_tables/app/src/main/java/com/example/sqlite_two_tables/MainActivity.java
+++ SQlite_two_tables/app/src/main/java/com/example/sqlite_two_tables/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sqlite_two_tables;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
@@ -18,10 +19,69 @@ public class MainActivity extends AppCompatActivity {
     String[] people_name = {"Сергей", "Петр", "Юлия", "Наталия", "Руслан", "Галина", "Лера", "Василий"};
     int[] people_posid = {2, 3, 2, 2, 3, 1, 2, 4};
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        DbHelper dbHelper = new DbHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Cursor cursor;
+
+        Log.d(TAG, "----Table position----");
+        cursor = db.query("position", null, null, null, null, null,
+                null);
+        logCursor(cursor);
+        cursor.close();
+        Log.d(TAG, "---- ----");
+
+        Log.d(TAG, "----Table people----");
+        cursor = db.query("people", null, null, null, null, null,
+                null);
+        logCursor(cursor);
+        cursor.close();
+        Log.d(TAG, "---- ----");
+
+        Log.d(TAG, "INNER JOIN with rowQuery");
+        String sqlQuery = "select PL.name as Name, PS.name as Position, salary as Salary "
+                + "from people as PL "
+                + "inner join position as PS "
+                + "on PL.posid = PS.id "
+                + "where salary > ?";
+        cursor = db.rawQuery(sqlQuery, new String[] {"40000"});
+        logCursor(cursor);
+        cursor.close();
+        Log.d(TAG, "---- ----");
+
+        Log.d(TAG, "INNER LOIN with query");
+        String table = "people as PL inner loin position as PS on PL.posid = PS.id";
+        String[] columns = {"PL.name as Name", "PS.name as Position", "salary as Salary"};
+        String selection = "salary < ?";
+        String[] selectionArgs = {"40000"};
+        cursor = db.query(table, columns, selection, selectionArgs, null, null, null);
+        logCursor(cursor);
+        cursor.close();
+        Log.d(TAG, "---- ----");
+
+        dbHelper.close();
+    }
+    void logCursor (Cursor cursor) {
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                String str;
+                do {
+                    str = "";
+                    for (String cn : cursor.getColumnNames()) {
+                        str = str.concat(cn + " = " + cursor.getColumnIndex(cn) + "; ");
+                    }
+                    Log.d(TAG, str);
+                } while (cursor.moveToNext());
+            }
+        }else {Log.d(TAG, "Cursor is null");}
     }
 
     class DbHelper extends SQLiteOpenHelper {
@@ -36,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
             db.execSQL("create table position ("
                     + "id integer primary key, "
                     + "name text, "
-                    + "salary integer"
+                    + "salary integer" 
                     + ");");
             for(int i = 0; i < position_id.length; i++) {
                 contentValues.clear();
@@ -44,6 +104,19 @@ public class MainActivity extends AppCompatActivity {
                 contentValues.put("name", position_name[i]);
                 contentValues.put("salary", position_salary[i]);
                 db.insert("position", null, contentValues);
+            }
+
+            db.execSQL("create table people ("
+                    + "id integer primary key autoincrement, "
+                    + "name text, "
+                    + "posid integer"
+                     + ");");
+            for(int i = 0; i < people_name.length; i++) {
+                contentValues.clear();
+                contentValues.put("name", people_name[i]);
+                contentValues.put("posid", people_posid[i]);
+                db.insert("people", null, contentValues);
+
             }
         }
 
