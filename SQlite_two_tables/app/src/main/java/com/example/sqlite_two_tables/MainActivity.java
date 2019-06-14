60db8b96ce504da76109d27eab1e92b6889c8e90
Yury Kurashov <yurikurashov@gmail.com>
2019-06-14 23:42:13 +0300

    запосы из двух таблиц

--- SQlite_two_tables/app/src/main/java/com/example/sqlite_two_tables/MainActivity.java
+++ SQlite_two_tables/app/src/main/java/com/example/sqlite_two_tables/MainActivity.java
@@ -11,117 +11,120 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    final String TAG = "myLogs";
+    final String LOG_TAG = "myLogs";
+
     int[] position_id = {1, 2, 3, 4};
-    String[] position_name = {"Директор", "Программист", "Бухгалтер", "HR"};
-    int[] position_salary = {100000, 90000, 50000, 70000};
+    String[] position_name = {"Директор", "Программист", "Бухгалтер", "Охранник"};
+    int[] position_salary = {80000, 60000, 40000, 20000};
 
-    String[] people_name = {"Сергей", "Петр", "Юлия", "Наталия", "Руслан", "Галина", "Лера", "Василий"};
+    String[] people_name = {"Максим", "Сергей", "Руслан", "Наталья", "Иван", "Мария", "Светлана", "Григорий"};
     int[] people_posid = {2, 3, 2, 2, 3, 1, 2, 4};
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         DbHelper dbHelper = new DbHelper(this);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        SQLiteDatabase sqLiteDatabase = dbHelper.getWritableDatabase();
+
         Cursor cursor;
 
-        Log.d(TAG, "----Table position----");
-        cursor = db.query("position", null, null, null, null, null,
-                null);
+        Log.d(LOG_TAG, "---Table position---");
+        cursor = sqLiteDatabase.query("position", null, null, null, null, null, null);
         logCursor(cursor);
         cursor.close();
-        Log.d(TAG, "---- ----");
+        Log.d(LOG_TAG, "--- ---");
 
-        Log.d(TAG, "----Table people----");
-        cursor = db.query("people", null, null, null, null, null,
-                null);
+        Log.d(LOG_TAG, "---Table people---");
+        cursor = sqLiteDatabase.query("people", null, null, null, null, null, null);
         logCursor(cursor);
         cursor.close();
-        Log.d(TAG, "---- ----");
+        Log.d(LOG_TAG, "--- ---");
 
-        Log.d(TAG, "INNER JOIN with rowQuery");
+
+        Log.d(LOG_TAG, "---INNER JOIN with rawQuery---");
         String sqlQuery = "select PL.name as Name, PS.name as Position, salary as Salary "
                 + "from people as PL "
                 + "inner join position as PS "
                 + "on PL.posid = PS.id "
                 + "where salary > ?";
-        cursor = db.rawQuery(sqlQuery, new String[] {"40000"});
+        cursor = sqLiteDatabase.rawQuery(sqlQuery, new String[] {"40000"});
         logCursor(cursor);
         cursor.close();
-        Log.d(TAG, "---- ----");
+        Log.d(LOG_TAG, "--- ---");
 
-        Log.d(TAG, "INNER LOIN with query");
-        String table = "people as PL inner loin position as PS on PL.posid = PS.id";
+
+        Log.d(LOG_TAG, "---INNER JOIN with query---");
+        String table = "people as PL inner join position as PS on PL.posid = PS.id";
         String[] columns = {"PL.name as Name", "PS.name as Position", "salary as Salary"};
         String selection = "salary < ?";
         String[] selectionArgs = {"40000"};
-        cursor = db.query(table, columns, selection, selectionArgs, null, null, null);
+        cursor = sqLiteDatabase.query(table, columns, selection, selectionArgs, null, null, null);
         logCursor(cursor);
         cursor.close();
-        Log.d(TAG, "---- ----");
+        Log.d(LOG_TAG, "--- ---");
 
         dbHelper.close();
     }
-    void logCursor (Cursor cursor) {
+
+    void logCursor(Cursor cursor) {
         if (cursor != null) {
             if (cursor.moveToFirst()) {
                 String str;
                 do {
                     str = "";
                     for (String cn : cursor.getColumnNames()) {
-                        str = str.concat(cn + " = " + cursor.getColumnIndex(cn) + "; ");
+                        str = str.concat(cn + " = " + cursor.getString(cursor.getColumnIndex(cn)) + "; ");
                     }
-                    Log.d(TAG, str);
+                    Log.d(LOG_TAG, str);
                 } while (cursor.moveToNext());
             }
-        }else {Log.d(TAG, "Cursor is null");}
+        } else Log.d(LOG_TAG, "Cursor is null");
     }
 
     class DbHelper extends SQLiteOpenHelper {
-        public DbHelper(@androidx.annotation.Nullable Context context) {
+
+        public DbHelper(Context context) {
             super(context, "myDb", null, 1);
         }
 
         @Override
-        public void onCreate(SQLiteDatabase db) {
-            Log.d(TAG, "----Table onCreate----");
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+            Log.d(LOG_TAG, "---onCreate database---");
+
             ContentValues contentValues = new ContentValues();
-            db.execSQL("create table position ("
+
+            sqLiteDatabase.execSQL("create table position ("
                     + "id integer primary key, "
-                    + "name text, "
-                    + "salary integer" 
+                    + "name text, " + "salary integer"
                     + ");");
-            for(int i = 0; i < position_id.length; i++) {
+
+            for (int i = 0; i < position_id.length; i++) {
                 contentValues.clear();
                 contentValues.put("id", position_id[i]);
                 contentValues.put("name", position_name[i]);
                 contentValues.put("salary", position_salary[i]);
-                db.insert("position", null, contentValues);
+                sqLiteDatabase.insert("position", null, contentValues);
             }
 
-            db.execSQL("create table people ("
+            sqLiteDatabase.execSQL("create table people ("
                     + "id integer primary key autoincrement, "
                     + "name text, "
                     + "posid integer"
-                     + ");");
-            for(int i = 0; i < people_name.length; i++) {
+                    + ");");
+
+            for (int i = 0; i < people_name.length; i++) {
                 contentValues.clear();
                 contentValues.put("name", people_name[i]);
                 contentValues.put("posid", people_posid[i]);
-                db.insert("people", null, contentValues);
-
+                sqLiteDatabase.insert("people", null, contentValues);
             }
+
         }
 
         @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
         }
     }
